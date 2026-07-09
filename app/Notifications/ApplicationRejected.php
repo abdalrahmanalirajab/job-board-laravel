835ec5b4932fdbeb9d8a0c5afb46d3102c7f5bc8
abdalrahmanalirajab <moaliabody@gmail.com>
2026-07-09 23:53:44 +0300

    Fix notifications not sending: remove ShouldQueue to process synchronously

--- app/Notifications/ApplicationRejected.php
+++ app/Notifications/ApplicationRejected.php
@@ -3,14 +3,11 @@
 namespace App\Notifications;
 
 use App\Models\Application;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ApplicationRejected extends Notification implements ShouldQueue
+class ApplicationRejected extends Notification
 {
-    use Queueable;
 
     public function __construct(public Application $application)
     {
--- app/Notifications/JobRejected.php
+++ app/Notifications/JobRejected.php
@@ -3,14 +3,11 @@
 namespace App\Notifications;
 
 use App\Models\JobListing;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class JobRejected extends Notification implements ShouldQueue
+class JobRejected extends Notification
 {
-    use Queueable;
 
     public function __construct(
         public JobListing $jobListing,
